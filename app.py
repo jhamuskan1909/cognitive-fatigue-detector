9ae6a0e481581810590359ccb1e439f348fe5657
jhamuskan1909 <jhamuskan1909@gmail.com>
2026-04-23 01:06:39 +0530

    fix port for railway

--- app.py
+++ app.py
@@ -222,5 +222,4 @@ def features():
     return jsonify(feature_importances)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
